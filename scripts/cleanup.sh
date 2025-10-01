f5f4d34b6b9857d2a48d9b56edad3831d12f057b
nyambogahezron <hezzyn1@gmail.com>
2025-10-02 01:42:30 +0300

    Remove pnpm workspace configuration and update cleanup script to reflect changes in lock file management

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Delete all node_modules directories
-echo "Cleaning up node_modules and dist directories, and pnpm-lock.yaml files..."
+echo "Cleaning up node_modules and dist directories, and bun.lock files..."
 find . -type d -name "node_modules" -prune -exec rm -rf '{}' +
 
 # Delete all dist directories
 find . -type d -name "dist" -prune -exec rm -rf '{}' +
 
-# Delete all pnpm-lock.yaml files
-find . -type f -name "pnpm-lock.yaml" -exec rm -f '{}' +
+# Delete all bun.lock files
+find . -type f -name "bun.lock" -exec rm -f '{}' +
 echo "Cleanup complete."
